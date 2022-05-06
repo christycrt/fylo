c0931bc233de8fb082b2ad66bc11451a1687c452
christycrt <christycrt@gmail.com>
2022-05-06 23:13:44 +0700

    [add] init project with react+typescript+scss+tailwind

---
+++ fylo-fe/src/App.tsx
@@ -0,0 +1,25 @@
+import "./styles/App.scss";
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
