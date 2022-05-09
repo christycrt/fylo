34983f3a2375f2151b9345b68c9d961542b95415
christycrt <christycrt@gmail.com>
2022-05-09 23:13:31 +0700

    [ADD] signup box

---
+++ fylo-fe/src/components/SignupBox.tsx
@@ -0,0 +1,14 @@
+const SignupBox = () => {
+  return (
+    <div className="bg-dark-blue">
+      <h2>Get early access today</h2>
+      <p>
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+    </div>
+  );
+};
+
+export default SignupBox;
